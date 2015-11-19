b6eb37d8236adfd658c15b3134b2f0cf72df8644
Alex Xu <a.i.xu@me.com>
2015-11-19 13:06:13 +0800

    Initial Commit

---
+++ WiFi RSSI Recorder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WiFi RSSI Recorder
+//
+//  Created by Alex Xu on 11/19/15.
+//  Copyright © 2015 Alex Xu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WiFi RSSI Recorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WiFi RSSI Recorder
+//
+//  Created by Alex Xu on 11/19/15.
+//  Copyright © 2015 Alex Xu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
